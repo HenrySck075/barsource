a37e688de17443df763512db6731245078617c45
yoshiko <email.is.not.vaild9@gmail.com>
2026-03-23 09:45:39 +0700

    j

---
+++ dart/lib/src/foundation/const.dart
@@ -0,0 +1,7 @@
+/// The epsilon of tolerable double precision error.
+///
+/// This is used in various places in the framework to allow for floating point
+/// precision loss in calculations. Differences below this threshold are safe to
+/// disregard.
+const double precisionErrorTolerance = 1e-10;
+
